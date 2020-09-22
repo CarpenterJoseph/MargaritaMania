0c167e5e7d2c16c9bc850d0cde9f70395dfeaa6f
Haahr2111 <36225747+Haahr2111@users.noreply.github.com>
2020-09-22 13:18:47 +0200

    It stores things but doesn't print

--- db.js
+++ db.js
@@ -46,6 +46,69 @@ var db = event.target.result;
 };
 };
 
+rDB.createRecipe = function(name, description, ingredients){
+    var db = datastore;
+    var date = new Date();
+    var timestamp = date.getTime();
+    
+    var recipeItem = { Name: name, Description: description, Ingredients: ingredients , recipeId: timestamp};
+    // Initiate a new transaction.
+    var transaction = db.transaction(["recipe"], "readwrite");
+    // Get the datastore.
+    var objStore = transaction.objectStore("recipe");
+   
 
+    // Create an object for the todo item.
+
+    // Create the datastore request.
+    const request = objStore.add(recipeItem);
+    console.log("Object store:" + JSON.stringify(objStore))
+
+    // Handle a successful datastore put.
+    request.onsuccess = () => {
+      console.log("Successfully Saved to do item: " + JSON.stringify(recipeItem));
+    };
+
+    // Execute the callback function.
+    transaction.oncomplete = () => {
+      console.log("Transaction completed on the database");
+    };
+
+    // Handle errors.
+    transaction.onerror = () => {
+      console.log("Transaction error happened on database");
+    };
+}
+
+rDB.fetchRecipes = function(callback){
+    console.log("Fetching function");
+
+    var db = datastore;
+    var transaction = db.transaction(["recipe"], "readwrite");
+    var objStore = transaction.objectStore("recipe");
+
+    var keyRange = IDBKeyRange.lowerBound(0);
+    var cursorRequest = objStore.openCursor(keyRange);
+
+    var recipes = [];
+
+    transaction.oncomplete = function (e) {
+        callback(recipes);
+    };
+
+    cursorRequest.onsuccess = function (e) {
+      var result = e.target.result;
+
+      if (!!result == false) {
+        return;
+      }
+
+      recipes.push(result.value);
+
+      result.continue();
+    };
+
+    cursorRequest.onerror = rDB.onerror;
+}
 return rDB
 })();
--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 window.onload=function(){
   recipeDB.open()
+
+
+  var newRecipeForm = document.getElementById("new-recipe-form");
+  var newInputName = document.getElementById("new-recipe");
+  var newInputDes = document.getElementById("new-recipe-description");
+  var newInputIng = document.getElementById("new-recipe-ingredients");
+
+   // Handle new item form submissions.
+   newRecipeForm.onsubmit = function (e) {
+    e.preventDefault();
+
+    // Get the text.
+    var name = newInputName.value;
+    var des = newInputDes.value;
+    var ing = newInputIng.value;
+
+    // Reset the input field.
+    newInputName.value = "";
+    newInputDes.value = "";
+    newInputIng.value = "";
+
+    recipeDB.createRecipe(name,  des, ing);
+    // Don't send the form.
+    refreshRecipes()
+    return false;
+  };
+
+};
+
+function refreshRecipes(){
+recipeDB.fetchRecipes(function(recipes){
+  console.log("This is recipees" + recipes)
+  var recipeList = document.getElementById("recipeslist");
+  recipeList.innerHTML = "";
+
+  recipes.array.forEach(recipeElement => {
+
+    console.log("Hey im the recipe"+ recipeElement);
+
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+      span.innerHTML = recipeElement.Name;
+      li.appendChild(span);
+      recipeList.appendChild(li);
+
+  });
+})
 }
+
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {

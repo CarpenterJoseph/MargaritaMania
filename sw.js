0dc15b079487ef32491ff13a138bde4ee53d0e6a
Haahr2111 <36225747+Haahr2111@users.noreply.github.com>
2020-10-07 10:36:53 +0200

    fuckoff onesignal

--- sw.js
+++ sw.js
@@ -17,8 +17,6 @@ self.addEventListener('install', event => {
                 '/./index.html',
                 '/./index.js',
                 '/./style.css',
-                '/./OneSignalSDKUpdateWorker.js',
-                '/./OneSignalSDKWorker.js',
                 '/images/kisspng-margarita-cocktail-mojito-cointreau-martini-margarita-5ac2b8fbd7f542.6731904215227107798846.png'
             ]);
         })

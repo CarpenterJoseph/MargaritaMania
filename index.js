8756515c3141faa363c7f50a18fc6dd670281f5a
Haahr2111 <36225747+Haahr2111@users.noreply.github.com>
2020-10-07 11:21:32 +0200

    fuckoff DB from sw

--- index.js
+++ index.js
@@ -32,55 +32,6 @@ navigator.serviceWorker.ready.then(function(registration){
   registration.sync.register('send-messages');
 })
 */
-window.onload = function () {
-  recipeDB.open();
-
-  var newRecipeForm = document.getElementById("new-recipe-form");
-  var newInputName = document.getElementById("new-recipe");
-  var newInputDes = document.getElementById("new-recipe-description");
-  var newInputIng = document.getElementById("new-recipe-ingredients");
-
-  // Handle new item form submissions.
-  newRecipeForm.onsubmit = function (e) {
-    e.preventDefault();
-
-    // Get the text.
-    var name = newInputName.value;
-    var des = newInputDes.value;
-    var ing = newInputIng.value;
-
-    // Reset the input field.
-    newInputName.value = "";
-    newInputDes.value = "";
-    newInputIng.value = "";
-
-    recipeDB.createRecipe(name, des, ing, refreshRecipes);
-    // Don't send the form.
-    return false;
-  };
-
-  refreshRecipes();
-};
-
-function refreshRecipes() {
-  recipeDB.fetchRecipes(function (recipes) {
-
-    var recipeList = document.getElementById("recipeslist");
-    recipeList.innerHTML = "";
-
-    recipes.forEach((recipeElement) => {
-
-      var li = document.createElement("li");
-      var span = document.createElement("span");
-
-      var returnString = recipeElement.Name + " - Description: " +  recipeElement.Description + " - Ingredients: " + recipeElement.Ingredients
-      span.innerHTML = returnString
-      
-      li.appendChild(span);
-      recipeList.appendChild(li);
-    });
-  });
-}
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
